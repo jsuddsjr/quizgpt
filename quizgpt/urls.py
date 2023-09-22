b3ee9d8d86b45969e8804027b2adb95b702b2f43
John Sudds <jsuddsjr@users.noreply.github.com>
2023-09-21 23:37:47 -0700

    pyproject.toml updates

--- quizgpt/urls.py
+++ quizgpt/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+admin.autodiscover()
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("hello/", views.hello),
+    path("admin/", admin.site.urls),
 ]
